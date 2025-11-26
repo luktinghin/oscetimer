d30c3ecea1964a44ceeb98bccde7933fd362ff9b
Terence Luk <34789390+luktinghin@users.noreply.github.com>
2025-11-26 18:43:43 +0800

    .

---
+++ main.js
@@ -0,0 +1,14 @@
+// Grab our credentials from a .env file or environment variables
+require('dotenv').config();
+const {
+    DATABASE_URL,
+    SUPABASE_SERVICE_API_KEY
+} = process.env;
+
+// Connect to our database
+const { createClient } = require('@supabase/supabase-js');
+const supabase = createClient(DATABASE_URL, SUPABASE_SERVICE_API_KEY);
+
+function test() {
+	
+}
