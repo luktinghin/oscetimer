014ef940e4e77337a3648978ef5762970635c639
Terence Luk <34789390+luktinghin@users.noreply.github.com>
2025-11-30 12:23:23 +0800

    .

--- main.js
+++ main.js
@@ -8,6 +8,8 @@ var loop1 = null;
 var loop2 = null;
 var connections = new Array();
 var c_count = -1;
+var temporal = {};
+temporal.distance = 0;
 
 // receiver.html code below
        function initialize() {
@@ -27,6 +29,7 @@ var c_count = -1;
 
                         console.log('ID: ' + peer.id);
                         document.getElementById("ownID").innerHTML = peer.id;
+                        loadURL();
                         //recvId.innerHTML = "ID: " + peer.id;
                         //status.innerHTML = "Awaiting connection...";
                     });
@@ -63,15 +66,21 @@ var c_count = -1;
                 function ready(c_count) {
                     //admin receives data channel
                     connections[c_count].conn.on('data', function (data) {
-                        console.log("Data received from peer " + connections[c_count].conn.peer);
-                        document.getElementById("message").innerHTML = "Peer " + c_count + " says: " + data;
+                        if (data.slice(0,2)=="MS") {
+                            str = data.slice(2);
+                            console.log("Data received from peer " + connections[c_count].conn.peer);
+                            addMessage(str,"Peer #" + c_count);
+                        } else if (data.slice(0,2)=="CM") {
+                            str = data.slice(2);
+                            receiver_parsecommand(str);
+                        }
                     });
                     connections[c_count].conn.on('close', function () {
                         //status.innerHTML = "Connection reset<br>Awaiting connection...";
                         connections[c_count].conn = null;
                     });
                 }
-//from send html
+//from send html, this is for receiver of OSCEtimer
                 function join(inputvalue) {
                     // Close old connection
                     if (r_conn) {
@@ -86,16 +95,31 @@ var c_count = -1;
                     r_conn.on('open', function () {
                         document.getElementById("status").innerHTML = "Listener: connected to peer";
                         console.log("Connected to: " + r_conn.peer);
-
+                        r_conn.send("CMrequestsync");
                         // Check URL params for comamnds that should be sent immediately
                         //var command = getUrlParam("command");
                         //if (command)
                         //    conn.send(command);
                     });
                     // Handle incoming data (messages only since this is the signal sender)
-                    r_conn.on('data', function (data) {
-                        //addMessage("<span class=\"peerMsg\">Peer:</span> " + data);
-                        document.getElementById("message").innerHTML = data;
+                    r_conn.on('data', function (data) {                        
+                        if (data.slice(0,2) == "TS") {
+                            //time stream signal
+                            str = data.slice(2);
+                            temporal.distance = str * 1;
+                            temporal.destination = temporal.distance + Date.now();
+                            receiver_sync(temporal.distance);
+                            console.log(temporal);
+                            addMessage("Timer distance is " + converttime(Math.floor(temporal.distance/1000)),"Debug");
+                        } else if (data.slice(0,2) == "CM") {
+                            //command
+                            receiver_parsecommand(data.slice(2));
+                        } else if (data.slice(0,2) == "MS") {
+                            //message
+                            str = data.slice(2);
+                            addMessage(str);    
+                        }
+                        
                     });
                     r_conn.on('close', function () {
                         //status.innerHTML = "Connection closed";
@@ -115,6 +139,12 @@ function init(value) {
     }
 }
 
+function addMessage(param2,param1) {
+    el0 = document.getElementById("message");
+    if (param1 == undefined) param1 = "System";
+    el0.innerHTML += "<br>" + param1 + ": " + param2;
+}
+
 function init_receiver() {
     //get id
     inputvalue = document.getElementById("senderID").value;
@@ -129,21 +159,14 @@ function update() {
 }
 
 function display_timer() {
-    timer_time = 2*60 - Math.floor(time_in_s)
-    if (timer_time >= 0) {
-        timestring = converttime(timer_time);
+    temporal.distance = temporal.destination - Date.now();
+    timer_time_in_s = Math.floor(temporal.distance/1000);
+    if (timer_time_in_s >= 0) {
+        timestring = converttime(timer_time_in_s);
         document.getElementById("timer_display").innerHTML = timestring;
     } else {
         timestring = "Admin timer has reached zero.";
         document.getElementById("timer_display").innerHTML = "Time's up!";
-    }
-    if (c_count>=0) {
-        for (i=0;i<connections.length;i++) {
-            if (connections[i].conn != null) {
-                connections[i].conn.send(timestring);
-                console.log("sent");
-            }    
-        }        
     }
 }
 
@@ -161,12 +184,127 @@ function converttime(relativeclock) {
     }
 }
 
-function start_stopwatch() {
+function sender_sync() {
+    if (temporal.distance>0) {
+        if (connections.length>0) {
+            connections.forEach((el) => {
+                if (el.conn != null) el.conn.send("TS" + temporal.distance);
+            });
+        }
+    }
+}
+
+function sender_command(param) {
+    if (connections.length>0) {
+        connections.forEach((el) => {
+            if (el.conn != null) el.conn.send("CM" + param);
+        });
+    }
+}
+
+function receiver_sync(distance) {
+    start_stopwatch(distance);
+}
+
+function receiver_parsecommand(param) {
+    if (param == "pause") {
+
+    } else if (param == "reset") {
+
+    } else if (param == "requestsync") {
+        //this is received from receiver requesting sync
+        sender_sync();
+    }
+}
+
+function loadURL() {
+    //read URL
+    const queryString = window.location.search;
+    if (queryString == "") {
+        console.log('no URL params detected');
+    } else {
+        const urlParams = new URLSearchParams(queryString);
+        const inputString = urlParams.get("U");
+        if (inputString == "") {
+            console.log("url: U param empty");
+        } else {
+            console.log(inputString);
+            addMessage("Auto-connected via URL command.")
+            join(inputString);
+            init(1);
+        }
+    }
+}
+
+function sharefunction2() {
+    sharefunction(peer.id);
+}
+
+function copyfunction2(is_link) {
+    copyfunction(peer.id,is_link);
+}
+
+async function sharefunction(param) {
+    //receives user id
+    url = "http://timersync.netlify.app/?U=" + param;
+    try {
+        await navigator.share({url:url});
+    } catch(err) {
+        console.log(err);
+        copyfunction(url);
+    }
+}
+
+function copyfunction(param,is_link) {
+    var textarea = document.createElement("textarea");
+    if (is_link) {
+        str = "http://timersync.netlify.app/?U=" + param;    
+    } else {
+        str = param;
+    }
+    
+        textarea.textContent = str;
+        //alert(textarea.textContent);
+        textarea.style.position = "fixed";  // Prevent scrolling to bottom of page in Microsoft Edge.
+        document.body.appendChild(textarea);
+        textarea.select();
+        try {
+            addMessage(str + " - copied to clipboard");
+                //document.getElementById("sharedatatext").focus();
+                //document.getElementById("sharedatatext").select();
+                //document.getElementById("copycheckmark").classList.toggle("translate");
+                //setTimeout(function() {document.getElementById("copycheckmark").classList.toggle("translate")}, 3000);
+            return document.execCommand("copy");  // Security exception may be thrown by some browsers.
+        }
+        catch (ex) {
+            console.warn("Copy to clipboard failed.", ex);
+            return prompt("Copy to clipboard: Ctrl+C, Enter", text);
+        }
+        finally {
+            document.body.removeChild(textarea);
+        }
+}
+
+function start_stopwatch(distance) {
+    document.getElementById("div_timer").style.display = "block";
     offset = Date.now();
-    clearInterval(loop1);
+    console.log(offset);
+    if (distance == undefined) {
+        dur = document.getElementById("select_timer").value * 60 * 1000;
+        temporal.destination = dur + offset;
+    } else {
+        temporal.destination = distance + offset;
+    }
+    console.log(temporal.destination);
+    update();
+    display_timer();
+    //clearInterval(loop1);
     clearInterval(loop2);
-    loop1 = setInterval(update,500);
-    loop2 = setInterval(display_timer,500);
+    //loop1 = setInterval(update,500);
+    loop2 = setInterval(display_timer,100);
+    if (connections.length>0) {
+        sender_sync();
+    }
 }
 
 function reset_stopwatch() {
@@ -178,5 +316,5 @@ function reset_stopwatch() {
 
 function testsend1() {
     count = count + 1;
-    r_conn.send("Hello world " + count);
+    r_conn.send("MS"+"Hello world " + count);
 }
